f59cea4f9570c79bc9264ed59fb29a7b189dbaa9
Indrajith <indrajithmohanasundaram@gmail.com>
2023-01-21 12:31:36 +0530

    created a header with title and a button

---
+++ src/components/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Timer = () => {
+    const title = "Timer App";
+  return (
+    <div className='container'>
+        <Header title={title}/>
+    </div>
+  )
+}
+
+export default Timer;
